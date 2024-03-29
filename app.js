4c5a50f933e43306a64a55745e43c03b0e41680e
Theroyal21 <theroyal2103@gmail.com>
2024-03-28 21:03:50 -1200

    add timeout second

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 1000);
+  res.status(err.status || 10000);
   res.render('error');
 });
 
